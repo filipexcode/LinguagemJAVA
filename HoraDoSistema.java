684cf616033af189c9a595481079aa485ec755f2
Filipe Moura <100590351+filipexcode@users.noreply.github.com>
2023-09-17 13:12:16 -0300

    Create HoraDoSistema.java

---
+++ HoraDoSistema.java
@@ -0,0 +1,12 @@
+package com.mycompany.horadosistema;
+
+import java.util.Date;
+
+public class HoraDoSistema {
+
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("A Hora do sistema e");
+        System.out.println(relogio.toString());
+    }
+}
