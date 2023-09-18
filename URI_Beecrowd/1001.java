b10dc53d96136b0a7455a4e1c677388d8f0bf473
Filipe Moura <100590351+filipexcode@users.noreply.github.com>
2023-09-18 00:13:29 -0300

    Create 1001.java

---
+++ URI_Beecrowd/1001.java
@@ -0,0 +1,18 @@
+//Fazer a entrada de dois valores inteiros e imprimir a soma na saida;
+
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+      Scanner leitor = new Scanner(System.in);
+        int a = leitor.nextInt();
+        int b = leitor.nextInt();
+        int x = a + b;
+        System.out.println("X = " + x);
+ 
+    }
+ 
+}
